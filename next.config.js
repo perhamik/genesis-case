068b920b7f4343b29e9bc9e6a8e5adcaf8cc1e30
Denys Yaroshenko <jaroshenkodenis97@gmail.com>
2023-03-16 16:34:43 +0200

    config env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-};
+	reactStrictMode: true,
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'wisey.app',
+				port: '',
+				pathname: '/assets/images/**',
+			},
+		],
+	},
+}
